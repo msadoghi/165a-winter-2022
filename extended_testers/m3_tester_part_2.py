623eba558fb4648a29ac8997fc162ab5cc95030c
Sajjad Rahnama <sajjad.rahnama7@gmail.com>
2022-03-14 11:18:15 -0700

    M3 Extended Testes

--- extended_testers/m3_tester_part_2.py
+++ extended_testers/m3_tester_part_2.py
@@ -17,10 +17,10 @@ query = Query(grades_table)
 # dictionary for records to test the database: test directory
 records = {}
 
-number_of_records = 1000
+number_of_records = 5000
 number_of_transactions = 100
-number_of_operations_per_record = 10
-num_threads = 8
+number_of_operations_per_record = 15
+num_threads = 12
 
 keys = []
 records = {}
@@ -30,7 +30,17 @@ seed(3562901)
 for i in range(0, number_of_records):
     key = 92106429 + i
     keys.append(key)
-    records[key] = [key, randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20)]
+    records[key] = [
+        key,
+        randint(i * 20, (i + 1) * 20),
+        randint(i * 20, (i + 1) * 20),
+        randint(i * 20, (i + 1) * 20),
+        randint(i * 20, (i + 1) * 20),
+        randint(i * 20, (i + 1) * 20),
+        randint(i * 20, (i + 1) * 20),
+        randint(i * 20, (i + 1) * 20),
+        randint(i * 20, (i + 1) * 20),
+        randint(i * 20, (i + 1) * 20)]
     # print(records[key])
 
 transaction_workers = []
@@ -43,13 +53,10 @@ for i in range(num_threads):
     transaction_workers.append(TransactionWorker())
 
 
-
-
-
 # x update on every column
 for j in range(number_of_operations_per_record):
     for key in keys:
-        updated_columns = [None, None, None, None, None]
+        updated_columns = [None, None, None, None, None, None, None, None, None, None]
         for i in range(2, grades_table.num_columns):
             # updated value
             value = randint(0, 20)
@@ -58,15 +65,15 @@ for j in range(number_of_operations_per_record):
             original = records[key].copy()
             # update our test directory
             records[key][i] = value
-            transactions[key % number_of_transactions].add_query(query.select, grades_table, key, 0, [1, 1, 1, 1, 1])
+            transactions[key % number_of_transactions].add_query(query.select, grades_table, key, 0, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
             transactions[key % number_of_transactions].add_query(query.update, grades_table, key, *updated_columns)
+            updated_columns[i] = None
 print("Update finished")
 
 
-# add trasactions to transaction workers  
+# add trasactions to transaction workers
 for i in range(number_of_transactions):
     transaction_workers[i % num_threads].add_transaction(transactions[i])
-
 
 
 # run transaction workers
@@ -83,8 +90,8 @@ for key in keys:
     try:
         correct = records[key]
         query = Query(grades_table)
-        
-        result = query.select(key, 0, [1, 1, 1, 1, 1])[0].columns
+
+        result = query.select(key, 0, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1])[0].columns
         if correct != result:
             print('select error on primary key', key, ':', result, ', correct:', correct)
             score -= 1
