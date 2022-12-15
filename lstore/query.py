c288cf7645dbcbdcb7b94c986bf4206d74c77fcd
Mohammad Sadoghi <msadoghi@ucdavis.edu>
2022-12-15 14:08:15 -0800

    Update query.py

--- lstore/query.py
+++ lstore/query.py
@@ -9,45 +9,46 @@ class Query:
     Queries that succeed should return the result or True
     Any query that crashes (due to exceptions) should return False
     """
-
     def __init__(self, table):
         self.table = table
         pass
 
+    
     """
     # internal Method
     # Read a record with specified RID
     # Returns True upon succesful deletion
     # Return False if record doesn't exist or is locked due to 2PL
     """
-
     def delete(self, primary_key):
         pass
+    
+    
     """
     # Insert a record with specified columns
     # Return True upon succesful insertion
     # Returns False if insert fails for whatever reason
     """
-
     def insert(self, *columns):
         schema_encoding = '0' * self.table.num_columns
         pass
 
+    
     """
-    # Read a record with specified key
-    # :param index_value: the value of index you want to search
-    # :param index_column: the column number of index you want to search based on
-    # :param query_columns: what columns to return. array of 1 or 0 values.
+    # Read matching record with specified search key
+    # :param search_key: the value you want to search based on
+    # :param search_key_index: the column index you want to search based on
+    # :param projected_columns: what columns to return. array of 1 or 0 values.
     # Returns a list of Record objects upon success
     # Returns False if record locked by TPL
     # Assume that select will never be called on a key that doesn't exist
     """
-
-    def select(self, index_value, index_column, query_columns):
+    def select(self, search_key, search_key_index, projected_columns):
         pass
 
+    
     """
-    # Read a record with specified key
+    # Return a record with specified key
     # :param index_value: the value of index you want to search
     # :param index_column: the column number of index you want to search based on
     # :param query_columns: what columns to return. array of 1 or 0 values.
@@ -56,19 +57,19 @@ class Query:
     # Returns False if record locked by TPL
     # Assume that select will never be called on a key that doesn't exist
     """
-
     def select_version(self, index_value, index_column, query_columns, relative_version):
         pass
 
+    
     """
     # Update a record with specified key and columns
     # Returns True if update is succesful
     # Returns False if no records exist with given key or if the target record cannot be accessed due to 2PL locking
     """
-
     def update(self, primary_key, *columns):
         pass
 
+    
     """
     :param start_range: int         # Start of the key range to aggregate 
     :param end_range: int           # End of the key range to aggregate 
@@ -77,10 +78,10 @@ class Query:
     # Returns the summation of the given range upon success
     # Returns False if no record exists in the given range
     """
-
     def sum(self, start_range, end_range, aggregate_column_index):
         pass
 
+    
     """
     :param start_range: int         # Start of the key range to aggregate 
     :param end_range: int           # End of the key range to aggregate 
@@ -90,10 +91,10 @@ class Query:
     # Returns the summation of the given range upon success
     # Returns False if no record exists in the given range
     """
-
     def sum_version(self, start_range, end_range, aggregate_column_index, relative_version):
         pass
 
+    
     """
     incremenets one column of the record
     this implementation should work if your select and update queries already work
@@ -102,7 +103,6 @@ class Query:
     # Returns True is increment is successful
     # Returns False if no record matches key or if target record is locked by 2PL.
     """
-
     def increment(self, key, column):
         r = self.select(key, self.table.key, [1] * self.table.num_columns)[0]
         if r is not False:
